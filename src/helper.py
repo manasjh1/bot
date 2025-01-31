62d862b1ae11589b9886af83d3317bf84b0f813a
Manas Jha <manasjh1@gmail.com>
2025-01-31 06:26:22 +0530

    updates

---
+++ src/helper.py
@@ -0,0 +1,25 @@
+
+from dotenv import load_dotenv
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+
+
+load_dotenv()
+
+#Extract data from PDFs
+def load_pdf_file(data_path):
+    loader = DirectoryLoader(data_path, glob="*.pdf", loader_cls=PyPDFLoader)
+    documents = loader.load()
+    return documents
+
+# Splits the extracted text into smaller chunks
+def text_split(extracted_data):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
+    text_chunks = text_splitter.split_documents(extracted_data)
+    return text_chunks
+
+#Download the Embeddings from HuggingFace 
+def download_hugging_face_embeddings():
+    embeddings=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')  #this model return 384 dimensions
+    return embeddings
